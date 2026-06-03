fde0025284bf8d2b803fd690f9ba7392f4081cd0
SydneyUy <suy2@byupathway.edu>
2026-06-03 22:42:40 +0400

    remodeled css for temples html

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,11 +1,10 @@
 const moddate = new Date(document.lastModified);
 document.getElementById('modified-date').textContent = moddate.toLocaleDateString();
 
-const mainnav = document.querySelector('.navigation');
+const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
-hambutton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const isOpen = mainnav.classList.toggle('show');
-    hambutton.classList.toggle('show', isOpen);
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
 });
