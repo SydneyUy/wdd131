a74aaa12df1e1e6ea29956ec4b3fbf968f3eb35d
SydneyUy <suy2@byupathway.edu>
2026-06-03 22:48:33 +0400

    fixed hamburger menu, changed .navigation to nav

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,7 +1,7 @@
 const moddate = new Date(document.lastModified);
 document.getElementById('modified-date').textContent = moddate.toLocaleDateString();
 
-const mainnav = document.querySelector('.navigation')
+const mainnav = document.querySelector('nav')
 const hambutton = document.querySelector('#menu');
 
 hambutton.addEventListener('click', () => {
