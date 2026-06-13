bb567f77f28028d65f1c450609e0192add523387
SydneyUy <suy2@byupathway.edu>
2026-06-13 23:57:58 +0400

    fixed css for mobile view

--- project/scripts/oryx.js
+++ project/scripts/oryx.js
@@ -8,3 +8,54 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+const subjects = [
+  {
+    id: "subject",
+    name: "General inquiry",
+  },
+
+    {
+    id: "subject",
+    name: "Website suggestion",
+  },
+
+    {
+    id: "subject",
+    name: "Booking inquiry",
+  },
+
+    {
+    id: "subject",
+    name: "Complaint",
+  },
+];
+
+function renderSections(subjects) {
+    const div = document.querySelector("#subject");
+    let rows = `<option value="" disabled selected>Please select a subject</option>`;
+    for (const subject of subjects) {
+        rows += 
+        `
+        <option value="${subject.id}">${subject.name}</option>
+        
+        `
+    }
+    div.innerHTML = rows;
+}
+
+renderSections(subjects);
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    let reviewCount = localStorage.getItem("reviewCount");
+
+    reviewCount = reviewCount ? parseInt(reviewCount, 10) : 0;
+
+    reviewCount += 1;
+
+    localStorage.setItem("reviewCount", reviewCount);
+
+    console.log(`Total reviews completed: ${reviewCount}`);
+    
+});
