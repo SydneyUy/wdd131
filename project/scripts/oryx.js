d8bd7c4814c3032729db62ded78123798eea28e4
SydneyUy <suy2@byupathway.edu>
2026-06-13 15:49:16 +0400

    fixed home heading line

--- project/scripts/oryx.js
+++ project/scripts/oryx.js
@@ -1,3 +1,10 @@
 const moddate = new Date(document.lastModified);
 document.getElementById('modified-date').textContent = moddate.toLocaleString();
 
+const mainnav = document.querySelector('nav');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
